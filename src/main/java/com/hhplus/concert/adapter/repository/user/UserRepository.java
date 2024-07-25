10c647287aa3c1a0bf717cb863a8cc023e24de02
xodn__love@naver.com <xodn__love@naver.com>
2024-07-25 22:11:42 +0900

    사용자 조회시 비관락 조회 추가

--- src/main/java/com/hhplus/concert/adapter/repository/user/UserRepository.java
+++ src/main/java/com/hhplus/concert/adapter/repository/user/UserRepository.java
@@ -1,7 +1,13 @@
 package com.hhplus.concert.adapter.repository.user;
 
 import com.hhplus.concert.domain.model.user.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, String> , UserRepositoryCustom {
+public interface UserRepository extends JpaRepository<User, String>  {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u WHERE u.id = :userId")
+    User findByIdForUpdate(String userId);
 }
